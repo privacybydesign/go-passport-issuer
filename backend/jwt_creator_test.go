cf87b9c219c1081f2c3497b0a3cc8b8fa3c17f01
saravahdatipour <saravahdatipour@gmail.com>
2025-09-11 13:50:57 +0200

    JWT creator fails when it shouldn't, perhaps concurrency issues

--- backend/jwt_creator_test.go
+++ backend/jwt_creator_test.go
@@ -6,30 +6,31 @@ import (
 	"time"
 )
 
-var testPassportIssuanceRequest = models.PassportIssuanceRequest{
-	Photo:                "./test-data/testpasfoto.jpg",
-	DocumentNumber:       "X1234567",
-	DocumentType:         "Passport",
-	FirstName:            "Alice",
-	LastName:             "Johnson",
-	Nationality:          "NLD",
-	IsEuCitizen:          "true",
-	DateOfBirth:          time.Date(1990, time.June, 15, 0, 0, 0, 0, time.UTC),
-	YearOfBirth:          "1990",
-	DateOfExpiry:         time.Date(2030, time.June, 15, 0, 0, 0, 0, time.UTC),
-	Gender:               "F",
-	Country:              "Netherlands",
-	Over12:               "true",
-	Over16:               "true",
-	Over18:               "true",
-	Over21:               "true",
-	Over65:               "false",
-	ActiveAuthentication: "true",
-}
-
-var jc, _ = NewIrmaJwtCreator("./test-secrets/priv.pem", "passport_issuer", "pbdf-staging.pbdf.passport")
-
 func TestCreatingJwt(t *testing.T) {
+
+	var jc, _ = NewIrmaJwtCreator("./test-secrets/priv.pem", "passport_issuer", "pbdf-staging.pbdf.passport")
+
+	var testPassportIssuanceRequest = models.PassportIssuanceRequest{
+		Photo:                "./test-data/testpasfoto.jpg",
+		DocumentNumber:       "X1234567",
+		DocumentType:         "Passport",
+		FirstName:            "Alice",
+		LastName:             "Johnson",
+		Nationality:          "NLD",
+		IsEuCitizen:          "true",
+		DateOfBirth:          time.Date(1990, time.June, 15, 0, 0, 0, 0, time.UTC),
+		YearOfBirth:          "1990",
+		DateOfExpiry:         time.Date(2030, time.June, 15, 0, 0, 0, 0, time.UTC),
+		Gender:               "F",
+		Country:              "Netherlands",
+		Over12:               "true",
+		Over16:               "true",
+		Over18:               "true",
+		Over21:               "true",
+		Over65:               "false",
+		ActiveAuthentication: "true",
+	}
+
 	jwt, err := jc.CreateJwt(testPassportIssuanceRequest)
 	if err != nil {
 		t.Fatalf("failed to create jwt: %v", err)
